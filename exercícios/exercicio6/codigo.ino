0e3955bf4b300801ecb08e4672346d92d959f834
Ana Rinaldi <99336060+rinaldianaluisa@users.noreply.github.com>
2022-03-11 10:37:58 -0300

    Create codigo.ino

---
+++ exercícios/exercicio6/codigo.ino
@@ -0,0 +1,48 @@
+// C++ code
+//
+const int botao1=2;
+const int botao2=3;
+const int botao3=4;
+const int botao4=5;
+const int botao5=6;
+  
+int var1;
+int var2;
+int var3;
+int var4;
+int var5;
+
+void setup()
+{
+  pinMode(botao1, INPUT);
+  pinMode(botao2, INPUT);
+  pinMode(botao3, INPUT);
+  pinMode(botao4, INPUT);
+  pinMode(botao5, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  var1=digitalRead(botao1);
+  var2=digitalRead(botao2);
+  var3=digitalRead(botao3);
+  var4=digitalRead(botao4);
+  var5=digitalRead(botao5);
+ 
+  if(var1){
+   Serial.println("azul e amarelo"); 
+  }
+  if(var2){
+   Serial.println("palmeiras melhor time");
+  }
+  if(var3){
+   Serial.println("meu aniversário é em novembro"); 
+  }
+  if(var4){
+   Serial.println("fone de ouvido é essencial"); 
+  }
+  if(var5){
+   Serial.println("nao vivo sem celular"); 
+  }
+}
