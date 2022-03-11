38861a4a9f209b7ee03834828ccd2a9fd6168802
Ana Rinaldi <99336060+rinaldianaluisa@users.noreply.github.com>
2022-03-11 10:16:32 -0300

    Create codigo.ino

---
+++ exercícios/exercicio5/codigo.ino
@@ -0,0 +1,25 @@
+// C++ code
+//
+const int led1 = 3;
+const int botao1 = 2;
+
+int var1;
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(botao1, INPUT);
+}
+
+void loop()
+{
+  var1 =digitalRead(botao1);
+  
+  if(var1){
+    
+  digitalWrite(led1, LOW);
+  }else{
+  digitalWrite(led1, HIGH);
+  }
+  
+}
